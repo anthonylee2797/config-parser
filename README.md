93c7902b708ef2a910b547de5fcc117a2fbb2b8e
Anthony <anthonylee2797@gmail.com>
2020-09-24 12:25:05 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# CONFIG Parser
+
+This config parser can parse through config files given the edge cases provided in the take home.
+
+Edit the config.txt file as necessary and edit the CONFIG_FILE variable in configParser.js if testing a new config file.
+
+I have created a ConfigParser class with two methods, getConfig and getValue.
+getConfig will log the parsed config file to the console
+getValue will log the value provided the parameter to the console
+
+![exampleImg](/assets/example.png)
--- configParser.js
+++ configParser.js
@@ -14,21 +14,29 @@ class ConfigParser {
 
 const CONFIG_FILE = 'config.txt'
 
-
 const config = fs.readFileSync(`${__dirname}/${CONFIG_FILE}`, 'utf-8').split('\n')
 
 const configStore = {}
 
 config.forEach((line) => {
+  // ignores comments
   if (line.trim()[0] !== '#') {
+
     try {
+
+      // separates keys and value from each line
       let value = line.split(/=(.+)/)[1].trim()
       let key = line.split('=')[0].trim()
 
+      // assign value to true
       if (value === 'on' || value === 'yes' || value === 'true') {
         value = true
+
+        // assign value to false
       } else if (value === 'off' || value === 'no' || value === 'false') {
         value = false
+
+        // if value is a number, turn value into number
       } else if (!isNaN(value)) {
         value = parseFloat(value)
       }
@@ -41,9 +49,12 @@ config.forEach((line) => {
   }
 })
 
-const configStore2 = new ConfigParser(configStore)
-configStore2.getConfig()
-configStore2.getValue('server_id')
+const configGiven = new ConfigParser(configStore)
+
+// logs config to screen
+configGiven.getConfig()
+// logs config paramater value
+configGiven.getValue('server_id')
 
 
 
